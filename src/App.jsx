b07c8d10770b1bcc053528ad22eb79a76ee12081
takume31 <160777450+ttwfij@users.noreply.github.com>
2024-03-06 10:28:37 -0400

    atividade 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+export default function App() {
+const nome1 = "Weslley";
+const nome2 = "João Santos";
+return(
+  <div>
+      <h1>Hello World</h1>
+      <p>Desenvolvido por: {nome1}</p>
+      <p>Desenvolvido por: {nome2}</p>
+      </div>
+);
+}
