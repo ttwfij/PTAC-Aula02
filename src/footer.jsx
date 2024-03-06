9ec083b4c03fde4b8161a4cc557738e4a06bab6a
takume31 <160777450+ttwfij@users.noreply.github.com>
2024-03-06 10:18:23 -0400

    Atividade 1

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <p>Henrique Takume</p>
+      <p>Weslley Gueller de Lima Cunha</p>
     </footer>
   );
   };
