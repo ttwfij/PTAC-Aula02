f1d9d6209df51f89738c3ccb6e1045bd61b03089
takume31 <henrique.nakamura@estudante.ifms.edu.br>
2024-03-20 16:03:38 -0400

    ati1

--- src/Takume.jsx
+++ src/Takume.jsx
@@ -2,6 +2,8 @@ import "./globalsrc.css"
 import {useState} from "react";
 export default function Takume(){
     const [nome, setnome] = useState("")
+    const [idade, setidade] = useState("")
+    const [telefone, setele] = useState("")
     return(
         <main>
         <div className='main'>
@@ -11,23 +13,29 @@ export default function Takume(){
             <div className="tudodentro">
             <a>Isira seu nome:</a>
             <br/>
-            <input name="" id="1" anChange={(event)=> setnome(event.target.value)}/>
+            <input type="text" name="" id="nome" 
+            anChange={(event)=> setnome(event.target.value)}/>
             <br/>
             <br/>
             <a>Isira sua idade:</a>
             <br/>
-            <input name="" id="2" anChange={(event)=> setnome(event.target.value)}/>
+            <input type="text" name="" id="idade"  
+            anChange={(event)=> setidade(event.target.value)}/>
+            {nome}
             <br/>
             <br/>
             <a>Isira seu telefone:</a>
             <br/>
-            <input name="" id="3" anChange={(event)=> setnome(event.target.value)}/>
+            <input type="text" name="" id="telefone"  
+            anChange={(event)=> setele(event.target.value)}/>
+            {idade}
             <br/>
             <button className="botao"><h4 className="botaol">Salvar dados</h4></button>
+            {telefone}
             </div>
             </div>
             <footer className="rodape">
-                <h1>Seja bem vindo</h1>
+            <h1>Seja bem vindo</h1>
             </footer>
         </main>
     )
