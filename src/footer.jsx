ddd3b6b5b658ba53bb73c91f75a0a5af284f75e0
takume31 <ttwfih65@gmail.com>
2024-04-03 14:53:00 -0400

    teste

--- src/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer>
-      <p>Weslley Gueller de Lima Cunha</p>
-    </footer>
-  );
-  };
--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Takume from './Takume.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <Takume/>
-  </React.StrictMode>
-)
